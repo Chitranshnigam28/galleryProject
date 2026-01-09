b8f9622cf9edb7304a5e821fdca3eab7025d48a2
chitransh nigam <chitranshnigam28@gmail.com>
2026-01-08 21:33:20 -0800

    pagination logic added

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,18 @@ import axios from 'axios'
 
 function App() {
   const [userData, setUserData] = useState([])
-
+  const [index,setIndex]=useState(1)
   const getData=async ()=>{
-    const response=await axios.get('https://picsum.photos/v2/list?page=3&limit=21');
+    const response=await axios.get(`https://picsum.photos/v2/list?page=${index}&limit=21`);
     setUserData(response.data)
     console.log(userData)
   }
 
+  
+
   useEffect(()=>{
     getData();
-  },[])
+  },[index])
 
   let printUserData=<h3 className='text-gray-600'>No data available</h3>;
   
@@ -41,7 +43,18 @@ function App() {
     <>
       <div className="bg-black h-screen text-white p-4 overflow-auto ">
        
-        <div className="flex flex-wrap gap-4">{printUserData}</div>
+        <div className="flex flex-wrap gap-4 justify-center">{printUserData}</div>
+        <div className="flex items-center justify-center mt-4 gap-4">
+          <button className='bg-amber-400 text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold text-black' onClick={()=>{
+            setIndex(index-1);
+
+            console.log('prev clicked '+index)
+          }}>Prev</button>
+          <button className='bg-amber-400 text-sm cursor-pointer active:scale-95 rounded px-4 py-2 font-semibold text-black' onClick={()=>{
+            setIndex(index+1);
+            console.log('next clicked'+index)
+          }}>Next</button>
+        </div>
       </div>
     </>
   )
